566c9b235b796cc2c21d1b2618420cd53840cddc
joshverma <joshverma72@gmail.com>
2023-08-10 23:39:41 -0400

    add station list gen script

---
+++ create_station_list.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import json
+import requests
+
+station_json = requests.get('https://gbfs.lyft.com/gbfs/2.3/bkn/fr/station_information.json').json()
+station_list = station_json.get('data').get('stations')
+
+df = pd.DataFrame(station_list)
+df.to_csv('citibike_station_list.csv')
+
