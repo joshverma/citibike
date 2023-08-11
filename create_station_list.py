d784e1f7c924396cb4afa792a1757203a2bf13ce
joshverma <joshverma72@gmail.com>
2023-08-11 01:10:52 -0400

    unique station ids

--- create_station_list.py
+++ create_station_list.py
@@ -4,7 +4,12 @@ import requests
 
 station_json = requests.get('https://gbfs.lyft.com/gbfs/2.3/bkn/fr/station_information.json').json()
 station_list = station_json.get('data').get('stations')
+station_list_map = {}
 
-df = pd.DataFrame(station_list)
+for station in station_list:
+	if not station_list_map.get(station.get('station_id')):
+		station_list_map[station.get('station_id')] = station
+
+df = pd.DataFrame(station_list_map.values())
 df.to_csv('station_list.csv')
 
